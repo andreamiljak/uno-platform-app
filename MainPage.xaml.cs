ee0637b0e3457e02f750fe204dc37ddeda395957
andreamiljak <99652693+andreamiljak@users.noreply.github.com>
2023-03-15 16:41:53 +0100

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.UI.Xaml.Controls;
+using LiveChartsCore;
+using LiveChartsCore.SkiaSharpView;
+
+
+
+namespace App7
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
+
+public class ViewModel
+{
+    public ISeries[] Series { get; set; }
+        = new ISeries[]
+        {
+            new LineSeries<double>
+            {
+                Values = new double[] { 2, 1, 3, 5, 3, 4, 6 },
+                Fill = null
+            }
+        };
+}
